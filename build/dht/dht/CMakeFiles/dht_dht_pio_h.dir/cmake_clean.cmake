bce865122360e87a4e0fe218f067cf7e55e04774
Leon Hiemstra <leon@x220.de-schans.com>
2026-09-02 21:18:31 +0200

    deleted build dir

--- build/dht/dht/CMakeFiles/dht_dht_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dht_dht_pio_h"
-  "dht.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dht_dht_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
